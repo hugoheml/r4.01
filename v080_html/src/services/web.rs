2e0d345af64c086b08dc8ebab8c2b9c421b952ec
hugoheml <51268820+hugoheml@users.noreply.github.com>
2025-02-19 14:24:10 +0000

    make router

--- v080_html/src/services/web.rs
+++ v080_html/src/services/web.rs
@@ -4,7 +4,7 @@ use anyhow::Ok;
 use axum::Router;
 use async_trait::async_trait;
 
-use crate::{interfaces::lexicon::Lexicon, service::Service, storage::Storage, use_cases::VotingController};
+use crate::{interfaces::{lexicon::Lexicon, web_interfaces::{router::make_router, web_routers::WEB_ROUTES, AxumState}}, service::Service, storage::Storage, use_cases::VotingController};
 
 pub struct WebService {
 	address: SocketAddr,
@@ -14,7 +14,21 @@ pub struct WebService {
 #[async_trait]
 impl <Store: Storage + Send + Sync + Clone + 'static> Service<Store> for WebService {
 	fn new(port: u16, lexicon: Lexicon, controller: VotingController<Store>) -> Self {
-		todo!()
+		// make_router
+		let address = format!("0.0.0.0:{}", port).parse().unwrap();
+
+		let axum_state = AxumState::<Store>{
+			controller: controller,
+			routes: WEB_ROUTES,
+			lexicon: lexicon
+		};
+
+		let router = make_router(axum_state, &WEB_ROUTES);
+
+		WebService {
+			address,
+			router,
+		}
 	}
 	async fn serve(&mut self) -> Result<(), anyhow::Error> {
 		let listener = tokio::net::TcpListener::bind(&self.address).await.unwrap();
